dca7fbe35ee002fbea6a4068b18a947eb9070a7d
KwasiSeth <sethaboagye607@gmail.com>
2022-09-08 11:55:31 -0700

    another commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Printing a statement
+ *
+ * return : Always 0 (will be successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
