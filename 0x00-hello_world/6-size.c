e989e287328c3473c90cb093b988f049ce853536
KwasiSeth <sethaboagye607@gmail.com>
2022-09-08 12:50:09 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * reurn :always 0(Successful)
  */
-int main (void)
+int main(void)
 {
 	char a;
 	int b;
@@ -12,10 +12,10 @@ int main (void)
 	long long int d;
 	float e;
 
-	printf("Size of a char:%d byte(s)\n", sizeof(a));
-	printf("Size of an int:%d byte(s)\n", sizeof(b));
-	printf("Size of a long int:%d byte(s)\n", sizeof(c));
-	printf("Size of a long long int:%d byte(s)\n", sizeof(d));
-	printf("Size of a float:%d byte(s)\n", sizeof (e));
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
