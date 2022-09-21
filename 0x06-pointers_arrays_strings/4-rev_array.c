eff0abbdd87a0dcdc28a6eb6fa417e8ee74afcf5
KwasiSeth <sethaboagye607@gmail.com>
2022-09-21 14:08:53 -0700

    commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,7 @@
  *reverse_array - reverses an array of integers
  * @a: array to be reversed
  * @n: number of elements in the array
- * */
+ **/
 void reverse_array(int *a, int n)
 {
 	int i, j, tmp;
